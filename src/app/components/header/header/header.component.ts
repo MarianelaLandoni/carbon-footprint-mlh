67634c5902fd639b8748369997c28a9e0575d31f
Marianela Landoni <mlandonih@hotmail.com>
2024-11-04 17:18:57 +0100

    feat: add header

---
+++ src/app/components/header/header/header.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  @Output() openSidebar = new EventEmitter<void>();
+
+  
+ }
