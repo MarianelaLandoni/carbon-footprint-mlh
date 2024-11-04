91ef81a2b66a377cfae98fffd45565d99bdc7049
Marianela Landoni <mlandonih@hotmail.com>
2024-11-04 17:21:32 +0100

    feat: update app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('carbon-footprint-mlh');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, carbon-footprint-mlh');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,80 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Event, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PrimeNGConfig } from 'primeng/api';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'carbon-footprint-mlh';
+
+  showSidebar: boolean = false;
+
+  constructor(
+    private config: PrimeNGConfig,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.showSidebar = false;
+      }
+    });
+    this.config.setTranslation({
+      dayNames: [
+        'domingo',
+        'lunes',
+        'martes',
+        'miércoles',
+        'jueves',
+        'viernes',
+        'sábado',
+      ],
+      dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+      dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+      monthNames: [
+        'Enero',
+        'Febrero',
+        'Marzo',
+        'Abril',
+        'Mayo',
+        'Junio',
+        'Julio',
+        'Agosto',
+        'Septiembre',
+        'Octubre',
+        'Noviembre',
+        'Diciembre',
+      ],
+      monthNamesShort: [
+        'ene',
+        'feb',
+        'mar',
+        'abr',
+        'may',
+        'jun',
+        'jul',
+        'ago',
+        'sep',
+        'oct',
+        'nov',
+        'dic',
+      ],
+      today: 'Hoy',
+      clear: 'Borrar',
+      weekHeader: 'Sm',
+    });
+  }
+
+  openOrCloseSidebar() {
+    this.showSidebar = !this.showSidebar;
+  }
 }
