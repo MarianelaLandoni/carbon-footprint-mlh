15eeae45570dbdf4244a77080dc83d8095aab647
Marianela Landoni <mlandonih@hotmail.com>
2024-11-04 17:21:08 +0100

    feat(routes): add dashboard, measure and advice routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DashboardViewComponent } from './views/dashboard-view/dashboard-view.component';
+import { MeasureViewComponent } from './views/measure-view/measure-view.component';
+import { AdviceViewComponent } from './views/advice-view/advice-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'dashboard', component: DashboardViewComponent},
+    {path: 'measure', component: MeasureViewComponent},
+    {path: 'advice', component: AdviceViewComponent},
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+    
+];
