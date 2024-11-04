d1f3bcd2951d0de352f571fd7099b02997b1f8af
Marianela Landoni <mlandonih@hotmail.com>
2024-11-04 17:20:41 +0100

    feat: add providers to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideCharts(withDefaultRegisterables()), provideHttpClient(), provideAnimations()],
 };
