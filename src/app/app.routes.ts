dc602b22c39ba51cea72dea59689ffb4e0a2f51a
Marianela Landoni <mlandonih@hotmail.com>
2024-11-05 10:26:21 +0100

    feat: add load component to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,30 @@
 import { Routes } from '@angular/router';
-import { DashboardViewComponent } from './views/dashboard-view/dashboard-view.component';
-import { MeasureViewComponent } from './views/measure-view/measure-view.component';
-import { AdviceViewComponent } from './views/advice-view/advice-view.component';
 
 export const routes: Routes = [
-    {path: 'dashboard', component: DashboardViewComponent},
-    {path: 'measure', component: MeasureViewComponent},
-    {path: 'advice', component: AdviceViewComponent},
-    {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-    
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./views/dashboard-view/dashboard-view.component').then(
+        (c) => c.DashboardViewComponent
+      ),
+  },
+  {
+    path: 'measure',
+    loadComponent: () =>
+      import('./views/measure-view/measure-view.component').then(
+        (c) => c.MeasureViewComponent
+      ),
+  },
+  {
+    path: 'advice',
+    loadComponent: () =>
+      import('./views/advice-view/advice-view.component').then(
+        (c) => c.AdviceViewComponent
+      ),
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
 ];
